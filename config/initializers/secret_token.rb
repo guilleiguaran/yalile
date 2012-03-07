67769e27370b514a80d2642ed3cc318cfbf5f891
Arturo Blanco <arturoblanco700@gmail.com>
2012-03-06 23:15:42 -0300

    Creating a new project for yalile shop

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yalile::Application.config.secret_token = '4de19cb5a6dd8fc732d0e03096bd8ed22317c34c8d1eadb0d52bff5614c7f567fcd8c556b9a2483abc781ee1efb4be9012d81dc1edb0f79d843e347f088a551c'
