b5a3d4f499d90009dcecd5c53c0c0f761c25238b
Arturo Blanco <arturoblanco700@gmail.com>
2012-05-14 23:59:28 -0300

    Adding views for new product and listing products and filter products

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,16 +1,17 @@
 $(function(){
+
   $(".skin-select").msDropDown();
+
   $( ".datepicker-call" ).datepicker({
     changeMonth: true,
     changeYear: true
   });
-  
-  
+
   /*Multiselect*/
   $(".multiselect-call").multiselect({
-     selectedList: 4 // 0-based index
+    selectedList: 4 // 0-based index
   });
-  
+
   /*Tooltips*/
   $('.error-info').tipsy({
     gravity: $.fn.tipsy.autoNS,
@@ -18,43 +19,48 @@ $(function(){
     delayIn: 500,
     delayOut: 500
   });
-  
+
   /*min Height sidebar*/
   $('.content-main').css("min-height",$(window).height()-$('#header').height()-69);
-  $(window).resize(function(){      
+
+  $(window).resize(function(){
     $('.content-main').css("min-height",$(window).height()-$('#header').height()-69);
   })
-  
+
   $('.content-right').css("width",$(window).width()-$('.sidebar').width());
-  $(window).resize(function(){      
+
+  $(window).resize(function(){
     $('.content-right').css("width",$(window).width()-$('.sidebar').width());
   })
-  
+
   /*Dropdown*/
-  
+
   $('.config > a').on("click", function(e){
-   $(this).parents(".main").find(".show").removeClass("show")
-   $(this).parent().toggleClass("show");
-   e.preventDefault();
+    $(this).parents(".main").find(".show").removeClass("show")
+    $(this).parent().toggleClass("show");
+    e.preventDefault();
   });
+
   $('.config-box').mouseup(function(){
-   return false
+    return false
   });
+
   $(document).mouseup(function(e){
-   if($(e.target).parent(".config").length==0){
-     $(".config").removeClass("show")
-     $('.overlay-sidebar').css("z-index", "2");
-   }
+    if($(e.target).parent(".config").length==0){
+      $(".config").removeClass("show")
+      $('.overlay-sidebar').css("z-index", "2");
+    }
   });
-  
-  
+
   /*Add New App*/
   $(".box-app").addClass("hidden");
+
   $('.add-app').click(function(event){
     event.stopPropagation();
     $(".box-app").addClass("open");
     $(".add-app").addClass("hidden");
   });
+
   $('.cancel-app').click(function(event){
     event.stopPropagation();
     $(".add-app").removeClass("hidden");
@@ -63,7 +69,7 @@ $(function(){
     $(".add-app").addClass("open");
     $(".add-app").removeClass("open");
   });
-  
+
   /*Edit App*/
   $('.btn-edit').click(function(event){
     event.stopPropagation();
@@ -74,6 +80,7 @@ $(function(){
     $(".skin-select").msDropDown();
     $("input.url").removeAttr('disabled', 'disabled')
   });
+
   $('li .cancel-app.edit').click(function(event){
     event.stopPropagation();
     $(".btn-edit").removeClass("hidden");
@@ -84,12 +91,11 @@ $(function(){
     $(".skin-select").msDropDown();
     $("input.url").attr('disabled', 'disabled')
   });
-  
+
   /*Overlay sidebar*/
   $('.nav-bar .config').click(function(event){
     event.stopPropagation();
     $('.overlay-sidebar').css("z-index", "3");
   });
-  
-  
+
 });
