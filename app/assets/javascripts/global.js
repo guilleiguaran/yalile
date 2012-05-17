c6b6ffecf8e0984d9b01e58ee47a7028501f9511
Arturo Blanco <arturoblanco700@gmail.com>
2012-05-17 20:43:46 -0300

    Finishing error shown on form

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,85 +1,95 @@
-$(function(){
+var globalJavascriptFile = (function  () {
 
-  $(".skin-select").msDropDown();
+  var globalJavascriptFile = {};
 
-  $(".datepicker-call").datepicker({
-    changeMonth: true,
-    changeYear: true
+  $(document).ready(function  () {
+
+    jQueryValidationsDefaults();
+
+    $(".skin-select").msDropDown();
+
+    $(".datepicker-call").datepicker({
+      changeMonth: true,
+      changeYear: true
+    });
+
+    /*Multiselect*/
+    $(".multiselect-call").multiselect({
+      selectedList: 4
+    });
+
+
+    /*min Height sidebar*/
+
+    $(window).resize(function(){
+      $('.content-main').css("min-height",$(window).height()-$('#header').height()-69);
+      $('.content-right').css("width",$(window).width()-$('.sidebar').width());
+    })
+
+    $('.content-main').css("min-height",$(window).height()-$('#header').height()-69);
+
+    $('.content-right').css("width",$(window).width()-$('.sidebar').width());
+
+    /*Dropdown*/
+
+    $('.config > a').on("click", function(e){
+      $(this).parents(".main").find(".show").removeClass("show")
+      $(this).parent().toggleClass("show");
+      e.preventDefault();
+    });
+
+    $('.config-box').mouseup(function(){
+      return false
+    });
+
+    $(document).mouseup(function(e){
+      if($(e.target).parent(".config").length==0){
+        $(".config").removeClass("show")
+        $('.overlay-sidebar').css("z-index", "2");
+      }
+    });
+
+    /*Overlay sidebar*/
+    $('.nav-bar .config').click(function(event){
+      event.stopPropagation();
+      $('.overlay-sidebar').css("z-index", "3");
+    });
+
   });
 
-  /*Multiselect*/
-  $(".multiselect-call").multiselect({
-    selectedList: 4
-  });
+  var jQueryValidationsDefaults = function  () {
+    $.validator.setDefaults({
+      highlight: function (element, errorClass) {
+        var inputTextElement = $(element);
+        inputTextElement.parent("fieldset").addClass("show-error");
+        inputTextElement.addClass("error-message");
+        inputTextElement.next("a.error-info").remove();
+      },
+      unhighlight: function (element, errorClass) {
+        var inputTextElement = $(element);
+        inputTextElement.parent("fieldset").removeClass("show-error");
+        inputTextElement.removeClass("error-message");
+        inputTextElement.next("a.error-info").text("");
+      },
+      errorPlacement: function (error, element) {
+        error.attr({title: error.text()});
+        error.html("");
+        element.after(error);
+        jQueryErrorsTooltip();
+      },
+      errorClass: "error-info",
+      errorElement: "a"
+    });
+  };
 
-  /*Tooltips
-  $('.error-info').tipsy({
+  var jQueryErrorsTooltip = function  () {
+    $('.error-info').tipsy({
     gravity: $.fn.tipsy.autoNS,
     html: true,
     delayIn: 500,
     delayOut: 500
-  });*/
+    });
+  };
 
-  /*min Height sidebar*/
-  $('.content-main').css("min-height",$(window).height()-$('#header').height()-69);
-
-  $(window).resize(function(){
-    $('.content-main').css("min-height",$(window).height()-$('#header').height()-69);
-  })
-
-  $('.content-right').css("width",$(window).width()-$('.sidebar').width());
-
-  $(window).resize(function(){
-    $('.content-right').css("width",$(window).width()-$('.sidebar').width());
-  })
-
-  /*Dropdown*/
-
-  $('.config > a').on("click", function(e){
-    $(this).parents(".main").find(".show").removeClass("show")
-    $(this).parent().toggleClass("show");
-    e.preventDefault();
-  });
-
-  $('.config-box').mouseup(function(){
-    return false
-  });
-
-  $(document).mouseup(function(e){
-    if($(e.target).parent(".config").length==0){
-      $(".config").removeClass("show")
-      $('.overlay-sidebar').css("z-index", "2");
-    }
-  });
-
-  //$(".box-app").addClass("hidden");
-
-  /*Edit App
-  $('.btn-edit').click(function(event){
-    event.stopPropagation();
-    $(this).addClass("hidden");
-    $("fieldset .show, fieldset.show-f, ul li fieldset.show-f span").removeClass("hidden");
-    $("ul li fieldset span.show, .no-show").addClass("hidden");
-    $(".list-cancel").removeClass("hidden");
-    $(".skin-select").msDropDown();
-    $("input.url").removeAttr('disabled', 'disabled')
-  });
-
-  $('li .cancel-app.edit').click(function(event){
-    event.stopPropagation();
-    $(".btn-edit").removeClass("hidden");
-    $(".no-show").removeClass("hidden");
-    $('ul.list-cancel').addClass("hidden");
-    $("fieldset .show, fieldset.show-f").addClass("hidden");
-    $("ul li fieldset span.show").removeClass("hidden");
-    $(".skin-select").msDropDown();
-    $("input.url").attr('disabled', 'disabled')
-  });*/
-
-  /*Overlay sidebar*/
-  $('.nav-bar .config').click(function(event){
-    event.stopPropagation();
-    $('.overlay-sidebar').css("z-index", "3");
-  });
-
-});
+  return globalJavascriptFile;
+})();
--- app/assets/javascripts/validation_forms/productForm.js
+++ app/assets/javascripts/validation_forms/productForm.js
@@ -12,19 +12,7 @@ var productForm = (function  () {
         "product[articles_attributes][0][size]":{ required: true, maxlength: 3 },
         "product[articles_attributes][0][in_stock]":{ required: true, maxlength: 4, range: [1, 100] },
         "product[articles_attributes][0][code]":{ required: true, maxlength: 30 }
-      },
-      highlight: function(element) {
-        var inputTextElement = $(element);
-        $(element).parent("fieldset").addClass("show-error");
-        $(element).addClass("show error-message");
-      },
-      unhighlight: function(element) {
-        var inputTextElement = $(element);
-        $(element).parent("fieldset").removeClass("show-error");
-        $(element).removeClass("show error-message");
-      },
-      errorClass: "error-info",
-      errorElement: "a"
+      }
     });
   }
 
