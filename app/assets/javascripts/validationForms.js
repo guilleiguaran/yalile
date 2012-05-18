fb94d96b028057462b8145716fad46bd429be6da
Arturo Blanco <arturoblanco700@gmail.com>
2012-05-18 00:08:12 -0300

    Moving and renaming file for handling all validation forms in one archive

---
+++ app/assets/javascripts/validationForms.js
@@ -0,0 +1,27 @@
+var validationForm = (function  () {
+  var validationForm = {};
+
+  var formForRender = "#form_for_html_render";
+  var productForm = "#product_form";
+
+  $(document).ready(function  () {
+    $(formForRender).on("change", formValidationFunctions);
+  });
+
+  var formValidationFunctions = function  () {
+    productFormValidations();
+  };
+
+  var productFormValidations = function  () {
+    $(productForm).validate({
+      rules:{
+        "product[name]":{ required: true },
+        "product[articles_attributes][0][size]":{ required: true, maxlength: 3 },
+        "product[articles_attributes][0][in_stock]":{ required: true, maxlength: 4, digits: true, range: [1, 100] },
+        "product[articles_attributes][0][code]":{ required: true, maxlength: 30 }
+      }
+    });
+  };
+
+  return validationForm;
+})();
