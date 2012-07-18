ebf2f3f048a0d7a13dafb88c0b3cf6286f45b635
Arturo Blanco <arturoblanco700@gmail.com>
2012-07-17 23:54:52 -0300

    Changing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
-  
+
   def formatted_time(date_object, format="")
     date_object.in_time_zone("Bogota").to_date.to_formatted_s(:rfc822)
   end
-  
+
   def page_entries_info(collection, options = {})
     entry_name = options[:entry_name] || (collection.empty?? 'entry' : collection.first.class.name.underscore.sub('_', ' '))
     if collection.num_pages < 2
       case collection.total_count
-      when 0; "No #{entry_name.pluralize} encontrado"
+      when 0; "Sin #{entry_name.pluralize} encontrados."
       when 1; "<strong>1</strong> #{entry_name.singularize} de <strong>#{collection.total_count}</strong>".html_safe
       else;   "<strong>#{collection.total_count} #{entry_name.pluralize} de #{collection.total_count}</strong>".html_safe
       end
@@ -21,5 +21,5 @@ module ApplicationHelper
       ]).html_safe
     end
   end
-  
+
 end
